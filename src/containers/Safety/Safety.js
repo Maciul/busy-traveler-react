efda0eb0ef4ab692d724c7b2b79cd5461c8dd309
Maciul <pawel.maciulewski@gmail.com>
2019-06-07 16:13:33 -0600

    finish up safety component

--- src/containers/Safety/Safety.js
+++ src/containers/Safety/Safety.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 // import classes from './InputForm.css';
-import { Container, Row, Col } from 'react-bootstrap';
+import {
+	Container, Row, Col, Image,
+} from 'react-bootstrap';
 import axios from 'axios';
+import styles from './Safety.module.css';
 // import StatDisplay from '../../components/StatDisplay/StatDisplay';
 
 class RestCountriesAPI extends Component {
@@ -32,17 +35,51 @@ class RestCountriesAPI extends Component {
 			});
 	}
 
+	riskIndex = (safetyScore) => {
+		switch (true) {
+			case safetyScore < 2.5:
+				return 'low';
+			case safetyScore < 3.5:
+				return 'medium';
+			case safetyScore < 4.5:
+				return 'high';
+			case safetyScore < 5:
+				return 'extreme';
+			default:
+				return 'none';
+		}
+	}
+
 	render() {
 		console.log(this.state.tugo);
+		console.log(this.riskIndex(this.state.travelAdvisory.score));
+		const riskScore = this.riskIndex(this.state.travelAdvisory.score);
 		const advisoryInfo = this.state.tugo.advisories && this.state.tugo.advisories.description;
 		return (
-			<Container>
-				<Row className="shadow p-3 mb-5 bg-white rounded">
+			<Container className="shadow p-3 mb-5 bg-white rounded">
+				<Row>
+					<Col className={styles.dangerImage}>
+						<Image src={`../../images/icons/danger-${riskScore}.png`} />
+					</Col>
+					<Col xs={10}>
+						<p className={styles.advisoryInfo}> {advisoryInfo} </p>
+						
+					</Col>
+				</Row>
+				<Row>
 					<Col>
-						<p> Rating: {this.state.travelAdvisory.score} / 5.0 </p>
-						<p> Information gathered from {this.state.travelAdvisory.sources_active} sources</p>
-						<p> {this.state.tugo.name}: {advisoryInfo} </p>
-						<p> More info to come soon... </p>
+					</Col>
+					<Col xs={5}>
+						<div className={styles.ratingInfo}>
+							<h3> Rating: </h3>
+							<h4> <b>{this.state.travelAdvisory.score}</b> / 5.0 </h4>
+						</div>
+					</Col>
+					<Col xs={5}>
+						<div className={styles.ratingInfo}>
+							<h3> Sources: </h3>
+							<p> Based on {this.state.travelAdvisory.sources_active} different sources </p>
+						</div>
 					</Col>
 				</Row>
 			</Container>
